13999d6ab905967424f707f866ea11866b4e50d1
bryanwlchl <89789320+bryanwlchl@users.noreply.github.com>
2021-09-27 09:04:19 +0200

    Update settings.py

--- amatha/settings.py
+++ amatha/settings.py
@@ -122,12 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles/img/')

b5dbccb36990c89b5f6d7a7c2d12241bb34c4c3d
Your name <you@example.com>
2021-10-12 21:22:44 +0200

    Update settings.py

--- amatha/settings.py
+++ amatha/settings.py
@@ -122,7 +122,7 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "staticfiles/")
 ]
 MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/img/')

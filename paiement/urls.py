d4f0cd7637c0972b5135332c7a98b07b5268c04d
bryanwlchl <89789320+bryanwlchl@users.noreply.github.com>
2021-10-14 14:18:19 +0200

    résumé pour paiement done

--- paiement/urls.py
+++ paiement/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import url
 from django.urls import path
-from .views import CreateCheckoutSessionView, ProductLandingPageView
+from .views import ProductLandingPageView
 
 app_name = 'paiement'
 
 urlpatterns = [
-    path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name="create-checkout-session"),
     path('', ProductLandingPageView.as_view(), name="landing-page")
 ]
--- paiement/views.py
+++ paiement/views.py
@@ -1,41 +1,26 @@
 import stripe
-from django.conf import settings
 from django.views.generic import TemplateView
-from django.http import JsonResponse
-from django.views import View
+
+from amatha import settings
+from cart.models import PanierItem
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class ProductLandingPageView(TemplateView):
     template_name = "paiement/pages/landing.html"
 
+
     def get_context_data(self, **kwargs):
+        panier = PanierItem.objects.filter(user = self.request.user)
         context = super(ProductLandingPageView, self).get_context_data(**kwargs)
+
+        prix_total = 0
+        for item in panier:
+            prix_total += item.price
+
         context.update({
-            "STRIPE_PUBLIC_KEY" : settings.STRIPE_PUBLIC_KEY
+            "prix_total" : prix_total,
+            "panier" : panier,
+            "STRIPE_PUBLIC_KEY" : settings.STRIPE_PUBLIC_KEY,
         })
         return context
-
-class CreateCheckoutSessionView(View):
-    def post(self, request, *args, **kwargs):
-        YOUR_DOMAIN = "http://127.0.0.1:8000"
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # TODO: replace this with the `price` of the product you want to sell
-                    # 'price': '{FAUT CHERCHER LE PRIX DU PANIER DU BOUG}',
-                    'quantity': 1,
-                },
-            ],
-            payment_method_types=[
-                'card',
-            ],
-            mode='payment',
-            # url qui nous mènes a la page du paiment reussi
-            success_url=YOUR_DOMAIN + '/success/',
-            # url qui nous mene a la page d'annulation de paiement
-            cancel_url=YOUR_DOMAIN + '/cancel/',
-        )
-        return JsonResponse({
-            'id': checkout_session.id
-        })

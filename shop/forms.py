a2b5be03ff2c14cb35263492eaa78ad666faa052
khloud95 <khloud.mhmd@eduge.ch>
2021-10-15 14:32:00 +0200

    changer les nom de form

--- shop/forms.py
+++ shop/forms.py
@@ -11,6 +11,20 @@ class PosteInsertVet(forms.ModelForm):
         fields = (
             'nom', 'description', 'stockMin', 'stockMax', 'stockDisponible', 'poid', 'produitActif', 'nomFichier',
             'sexe', 'typeTissu', 'couleur', 'taille',)
+        labels = {
+            'nom': 'Nom',
+            'description': 'Description',
+            'stockMin': 'Stock minimum',
+            'stockMax': 'Stock maximum',
+            'stockDisponible': 'Stock disponible',
+            'poid': 'Poid',
+            'produitActif': 'Produit actif',
+            'nomFichier': 'Nom fichier',
+            'sexe': 'Sexe',
+            'typeTissu': 'Type tissu',
+            'couleur': 'Couleur',
+            'taille': 'Taille'
+        }
 
 
 ### Form Insertion Produit Intérieur
@@ -20,6 +34,17 @@ class PosteInsertProInt(forms.ModelForm):
         fields = (
             'nom', 'description', 'stockMin', 'stockMax', 'stockDisponible', 'poid', 'produitActif', 'nomFichier',
             'type')
+        labels = {
+            'nom': 'Nom',
+            'description': 'Description',
+            'stockMin': 'Stock minimum',
+            'stockMax': 'Stock maximum',
+            'stockDisponible': 'Stock disponible',
+            'poid': 'Poid',
+            'produitActif': 'Produit actif',
+            'nomFichier': 'Nom fichier',
+            'type': 'Type'
+        }
 
 
 ### Form Insertion Produit Cosmétique
@@ -29,6 +54,19 @@ class PosteInsertProCos(forms.ModelForm):
         fields = (
             'nom', 'description', 'stockMin', 'stockMax', 'stockDisponible', 'poid', 'produitActif', 'nomFichier',
             'ingredient', 'gamme', 'categorie')
+        labels = {
+            'nom': 'Nom',
+            'description': 'Description',
+            'stockMin': 'Stock minimum',
+            'stockMax': 'Stock maximum',
+            'stockDisponible': 'Stock disponible',
+            'poid': 'Poid',
+            'produitActif': 'Produit actif',
+            'nomFichier': 'Nom fichier',
+            'ingredient': 'Ingrédients',
+            'gamme': 'Gamme',
+            'categorie': 'Catégorie'
+        }
 
 
 ### Form Insertion Atelier
@@ -38,6 +76,18 @@ class PosteInsertAtelier(forms.ModelForm):
         fields = (
             'titre', 'description', 'nbrPersonneMax', 'dateDebut', 'heureDebut', 'heureFin', 'adresse', 'prix',
             'AtelierActif', 'nomFichier')
+        labels = {
+            'titre' : 'Titre',
+            'description': 'Description',
+            'nbrPersonneMax': 'Nombre de personne',
+            'dateDebut': 'Date début',
+            'heureDebut': 'Heure début',
+            'heureFin': 'Heure fin',
+            'adresse': 'Adresse',
+            'prix': 'Prix',
+            'AtelierActif': 'Atelier actif',
+            'nomFichier': 'Nom fichier',
+        }
 
 
 # ###################### TEST INSERTION PHOTO ############################
@@ -55,6 +105,9 @@ class PosteUpdateStockVet(forms.ModelForm):
         model = Vetement
         fields = (
             'stockDisponible',)
+        labels = {
+            'stockDisponible': 'Stock disponible',
+        }
 
 
 ### Form Update Stock Produits Intérieurs
@@ -63,7 +116,9 @@ class PosteUpdateStockProInt(forms.ModelForm):
         model = ProduitInterieur
         fields = (
             'stockDisponible',)
-
+        labels = {
+            'stockDisponible': 'Stock disponible',
+        }
 
 ### Form Update Stock Vetêment
 class PosteUpdateStockProCos(forms.ModelForm):
@@ -71,6 +126,9 @@ class PosteUpdateStockProCos(forms.ModelForm):
         model = Cosmetique
         fields = (
             'stockDisponible',)
+        labels = {
+            'stockDisponible': 'Stock disponible',
+        }
 
 
 ### Form Update Prix
@@ -79,6 +137,12 @@ class PosteUpdatePrix(forms.ModelForm):
         model = Prix
         fields = (
             'montant', 'reduction', 'date',)
+        labels = {
+            'montant': 'Montant',
+            'reduction': 'Réduction',
+            'date': 'Date',
+        }
+
 
 ### Form Update Prix
 class PosteInsertPrix(forms.ModelForm):
@@ -86,6 +150,12 @@ class PosteInsertPrix(forms.ModelForm):
         model = Prix
         fields = (
             'montant', 'reduction', 'date', 'produit')
+        labels = {
+            'montant': 'Montant',
+            'reduction': 'Réduction',
+            'date': 'Date',
+            'produit': 'Nom produit'
+        }
 
 
 ### Form Désactiver un client
@@ -94,3 +164,6 @@ class PosteDesativerClient(forms.ModelForm):
         model = Client
         fields = (
             'clientActif',)
+        labels = {
+            'clientActif': 'Client actif'
+        }

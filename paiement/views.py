3e0423012f85f2bb19419824ea47a08b3db031c9
khloud95 <khloud.mhmd@eduge.ch>
2021-10-21 14:16:38 +0200

    enregistrement commande

--- paiement/views.py
+++ paiement/views.py
@@ -1,18 +1,22 @@
+from datetime import date
+from random import random
+
 import stripe
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from amatha import settings
 from cart.models import PanierItem
+from shop.models import LigneAtelierCommande, LigneProduitCommande, Commande
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
+
 
 class ProductLandingPageView(TemplateView):
     template_name = "paiement/pages/landing.html"
 
-
     def get_context_data(self, **kwargs):
-        panier = PanierItem.objects.filter(user = self.request.user)
+        panier = PanierItem.objects.filter(user=self.request.user)
         context = super(ProductLandingPageView, self).get_context_data(**kwargs)
 
         prix_total = 0
@@ -22,19 +26,54 @@ class ProductLandingPageView(TemplateView):
         prix_total = prix_total * 100
 
         context.update({
-            "prix_total" : prix_total,
-            "panier" : panier,
-            "STRIPE_PUBLIC_KEY" : settings.STRIPE_PUBLIC_KEY,
+            "prix_total": prix_total,
+            "panier": panier,
+            "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY,
         })
         return context
+
 
 def charge(request):
     panier = PanierItem.objects.filter(user=request.user)
     prix_total = 0
+    listeArticle = []
+
     for item in panier:
         prix_total += item.price
-
     prix_total = prix_total * 100
+
+    for element in panier:
+        clientId = request.user.client
+        comTotal = prix_total / 100
+        statut = 'En attente'
+        produit = element.product
+        atelier = element.atelier
+        listeArticle.append(element)
+        quantite = element.quantity
+        comNumero = random.randint(1, 1000)
+        comDate = date.today()
+
+    commande = Commande.objects.create(
+        clientId=clientId,
+        comNumero=comNumero,
+        comTotal=comTotal,
+        statut=statut,
+        comDate=comDate,
+    )
+
+    for art in listeArticle:
+        if art.product is not None:
+            ligneProCom = LigneProduitCommande.objects.create(
+                produit=art.product,
+                commande=commande,
+                quantite=art.quantity,
+            )
+        if art.atelier is not None:
+            ligneAteCom = LigneAtelierCommande.objects.create(
+                atelier=art.atelier,
+                commande=commande,
+                nbrPersonne=art.quantity
+            )
 
     if request.method == 'POST':
         charge = stripe.Charge.create(
@@ -43,4 +82,5 @@ def charge(request):
             description='amatha facture',
             source=request.POST['stripeToken']
         )
+
         return render(request, 'paiement/pages/charge.html')
